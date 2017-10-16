fbcca3209513a3327c9468fa404775edea37e333
Oleg Gromyak <oagromyak@gmail.com>
2017-10-16 16:37:54 +0300

    Remove anything, move to python3.6, start with new library

--- constants.example.py
+++ constants.example.py
@@ -1,23 +1,9 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-token = '123456789:dfghdfghdflugdfhg-77fwftfeyfgftre'  # bot access_token
-sn_stickers = ('CADAgADDwAu0BX', 'CAADA',
-               'CDAgADEQADfvu0Bh0Xd-rAg', 'CAADAgAADfvu0Bee9LyXSj1_fAg',)  # ids
-some2_stickers = ('CAADAKwADd_JnDFPYYarHAg', 'CAADAgADJmEyMU5rGAg')
-allowed_stickers = sn_stickers + some2_stickers
-max_allowed_stickers_in_row = 3
-default_probability = 0.01  # value hidden
-del_symbols = '`~1234567890!@#'  # symbols to ignore
-start_msg = 'я тут!'
-source_msg = ''
-min_sleep_time = 0.5
-max_sleep_time = 5
-sleep_multiplier = 2
-quotes_dict = {  # examples
-    (0.6, 'університет', 'университет'): """ну що тут сказати
-    цитата2
-    @, що Ви мали на увазі?""",  # before sending @ will be replaced by username or name
-    (0.75, sn_stickers): """стікер зі мною детектед
-    а я непогайно тут виглядаю
-    цитата3"""}
+TOKEN = '123456789:dfghdfghdflugdfhg-77fwftfeyfgftre'  # bot access_token
+
+ALLOWED_STICKERS = ['TheGus']  # just specify sticker set names
+
+START_MSG = 'Ready, steady, go!'
+SOURCE_MSG = 'Some text here'
--- main.py
+++ main.py
@@ -1,157 +1,48 @@
-# coding: utf-8
-from __future__ import unicode_literals
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from classes import StoppableThread
-import random
-import requests
-import Queue
-import time
-import logging
-import constants as c
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
 
+from aiotg import Bot, Chat
+
+import constants as c
+import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
-
-def start(bot, update):
-    if c.start_msg:
-        update.message.reply_text(c.start_msg)
+bot = Bot(api_token=c.TOKEN)
 
 
-def source(bot, update):
-    if c.source_msg:
-        update.message.reply_text(c.source_msg)
+@bot.command("start")
+def start(chat: Chat, match):
+    if c.START_MSG:
+        return chat.reply(c.START_MSG)
 
 
-def is_forwarded(message):
-    return message.forward_from or message.forward_from_chat
+@bot.command("whoami")
+def whoami(chat: Chat, match):
+    if chat.type == "private":
+        return chat.reply(chat.sender["id"])
 
 
-def delete_thread_func(stopped):
-    while not stopped():
-        try:
-            msg = delete_queue.get(False, 1)
-        except Queue.Empty:
-            continue
-        print 'deleting sticker:', msg['msg_id']
-        addr = 'https://api.telegram.org/bot%s/deleteMessage?chat_id=%s&message_id=%s' % (c.token,
-                                                                                          msg['chat_id'],
-                                                                                          msg['msg_id'])
-        r = requests.post(addr)
-        sleep_time = c.min_sleep_time
-        while r.status_code != requests.codes.ok:
-            print 'fail:', msg['msg_id'], r.status_code
-            r = requests.post(addr)
-            time.sleep(sleep_time)
-            if sleep_time <= c.max_sleep_time:
-                sleep_time *= c.sleep_multiplier
-        print 'success:', msg['msg_id']
+@bot.command("source")
+def source(chat: Chat, match):
+    return chat.send_text(c.SOURCE_MSG)
 
 
-def sticker(bot, update):
-    global last_sender_id, last_sender_stickers_in_row
-
-    if is_forwarded(update.message):
-        return
-
-    if update.message.chat.type == 'private':
-        print(update.message.sticker.file_id)
-    else:
-        if update.message.sticker.file_id not in c.allowed_stickers:
-            print(update.message.message_id)
-            delete_queue.put({'chat_id': update.message.chat.id, 'msg_id': update.message.message_id})
-        else:
-            sender_id = update.message.from_user.id
-            if sender_id != last_sender_id:
-                last_sender_id = sender_id
-                last_sender_stickers_in_row = 1
-            else:
-                last_sender_stickers_in_row += 1
-            if last_sender_stickers_in_row > c.max_allowed_stickers_in_row:
-                delete_queue.put({'chat_id': update.message.chat.id, 'msg_id': update.message.message_id})
-                return
-            sticker = update.message.sticker.file_id
-            r = random.random()
-            res = None
-            print r
-            for x in c.quotes_dict:
-                probability = x[0] if type(x[0]) is float else c.default_probability
-                if probability >= r:
-                    for var in filter(lambda t: type(t) is list or type(t) is tuple, x):
-                        if sticker in var:
-                            res = c.quotes_dict[x]
-                            break
-                if res:
-                    break
-            if res:
-                return reply_some(res, bot, update, reply_enabled=False)
-
-
-def text(bot, update):
-    if is_forwarded(update.message):
-        return
-
-    if update.message.chat.type == 'private':
-        return
-    else:
-        text = update.message.text.lower().strip().replace('  ', ' ')
-        for symbol in c.del_symbols:
-            text = text.replace(symbol, '')
-        r = random.random()
-        res = None
-        for x in c.quotes_dict:
-            probability = x[0] if type(x[0]) is float else c.default_probability
-            if probability >= r:
-                for var in filter(lambda t: type(t) is str or type(t) is unicode, x):
-                    if text == var or text.startswith(var + ' ') or text.endswith(' ' + var) or ' ' + var + ' ' in text:
-                        res = c.quotes_dict[x]
-                        break
-            if res:
-                break
-        if res:
-            return reply_some(res, bot, update)
-
-
-def reply_some(reply, bot, update, reply_enabled=True):
-    reply = reply.split('\n')
-    reply = random.choice(reply).strip()
-    if '@' in reply or not reply_enabled:
-        bot.send_message(chat_id=update.message.chat.id,
-                         text=reply.replace('@', update.message.from_user.name))
-    else:
-        update.message.reply_text(reply)
-
-
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"' % (update, error))
+@bot.handle("sticker")
+def sticker(chat: Chat, sticker: dict):
+    if chat.type == "private":
+        return chat.reply(f"Sticker set: {sticker['set_name']}, sticker id: {sticker['file_id']}")
+    elif sticker['set_name'] not in c.ALLOWED_STICKERS:
+        return bot.api_call('deleteMessage',
+                            chat_id=chat.id,
+                            message_id=chat.message['message_id'])
 
 
 def main():
-    delete_thread = StoppableThread(target=delete_thread_func)
-    delete_thread.start()
-
-    updater = Updater(c.token)
-
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("source", source))
-    dp.add_handler(MessageHandler(Filters.sticker, sticker))
-    dp.add_handler(MessageHandler(Filters.text, text))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-
-    updater.idle()
-    delete_thread.stop()
+    bot.run(debug=True)
 
 
 if __name__ == '__main__':
-    delete_queue = Queue.Queue()
-    last_sender_id = None
-    last_sender_stickers_in_row = 0
     main()
